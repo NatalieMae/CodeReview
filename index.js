22017bbc6fdc605dfb23508ac6e1798cfc1d3840
unknown <nataliemae263@gmail.com>
2022-01-16 00:16:38 -0600

    issue 4

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
-let x = 5
-let y = 8
+//let x = 5
+//let y = 8
 
 // function addNumbers(x, y) {
     // console.log(3+5)
 // }
 // console.log(x+y)
-const addNumbers = function {
-    console.log()
-}
+//const addNumbers = function {
+//    console.log()
+//}
+
+// let millimeters = 5 
+// let centimeters = "10"
+// let totalBitSize = Number(millimeters) + Number(centimeters);
+// console.log(totalBitSize) // "510"                
+let housePets = ["cat", "dog", "parrot", "turtle",]
+
+console.log(housePets[0])
+console.log(housePets[1])
--- index1.js
+++ index1.js
@@ -7,26 +7,26 @@
 
 
 //Create array of all Items purchased from SilverTrack
-let purchasedCars = [
+// let purchasedCars = [
 
-    { Type: 22, IdNumber: 21101, Model: 'S1', Insured: true, YearBuilt: '1922' },
-    { Type: 'StockCar', IdNumber: 4654, Model: 'S1', Insured: 'True', YearBuilt: '2011' },
-    //...
-  ]
+//     { Type: 22, IdNumber: 21101, Model: 'S1', Insured: true, YearBuilt: '1922' },
+//     { Type: 'StockCar', IdNumber: 4654, Model: 'S1', Insured: 'True', YearBuilt: '2011' },
+//     //...
+//   ]
   
-  //loop through purchasedCars array
-  for (//loop logic goes here ) {
+//   //loop through purchasedCars array
+//   for (//loop logic goes here ) {
   
-    //use typeof to get data type
-    let getTypeDataType = typeof purchasedCars[i].Type  //expecting String
-    if (getTypeDataType !== 'string') {
-      //if the data type is not a string, explicitly convert to string
-      purchasedCars[i].Type = String(purchasedCars[i].Type);
-    }
-      //continue checking the data type for each item in the array
+//     //use typeof to get data type
+//     let getTypeDataType = typeof purchasedCars[i].Type  //expecting String
+//     if (getTypeDataType !== 'string') {
+//       //if the data type is not a string, explicitly convert to string
+//       purchasedCars[i].Type = String(purchasedCars[i].Type);
+//     }
+//       //continue checking the data type for each item in the array
   
-      //add conversion logic to the correct datatype based on PART ONE insructions.
-  }
+//       //add conversion logic to the correct datatype based on PART ONE insructions.
+//   }
   
-  //output array with the required dataTypes
-  console.log(purchasedCars);
+//   //output array with the required dataTypes
+//   console.log(purchasedCars);
